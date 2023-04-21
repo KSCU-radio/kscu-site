2b4f37cff6a827146e456fceafa4ada34bf15ca5
Aidan Smith <asmith2@scu.edu>
2023-04-21 11:26:23 -0700

    Updated to use env variables

--- tina/config.ts
+++ tina/config.ts
@@ -3,11 +3,13 @@ import { blog_postFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const clientIDENV: string = process.env.clientId ?? "Could not find clientID";
+const tokenENV: string = process.env.token ?? "Could not find token";
 
 export default defineConfig({
   branch,
-  clientId: "ca5f5f38-18b4-4333-867e-433b4144341e", // Get this from tina.io
-  token: "7f7f1ee5a242ed1c1dd540548e7101400fa51b02", // Get this from tina.io
+  clientId: clientIDENV, // Get this from tina.io
+  token: tokenENV, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
@@ -15,7 +17,7 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
+      mediaRoot: "uploads",
       publicFolder: "static",
     },
   },
